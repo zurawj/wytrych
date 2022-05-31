6e93cf7abdde6c9f4729759889dc28c0beea7a75
zurawj <jakub.zurawski96@gmai.com>
2022-05-31 12:48:39 +0200

    temp

--- wytrych1/Wytrych.cs
+++ wytrych1/Wytrych.cs
@@ -13,7 +13,7 @@ namespace wytrych1
         public bool zlamanieWytrycha(int szansa)
         {
             Random rnd = new Random();
-           
+            int termmx = 0;
                 int los = rnd.Next(0, 99);
             if (los > szansa)
             {
