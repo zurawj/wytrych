bb390961316d983841ee035e7988f92bfa7f29fe
Waldemar Dacko <waldekd@softsystem.pl>
2022-06-07 21:29:59 +0200

    changes to be changed :)

--- wytrych1/Wytrych.cs
+++ wytrych1/Wytrych.cs
@@ -9,12 +9,15 @@ namespace wytrych1
     internal class Wytrych
     {
 
- 
+        //metoda z duzej litery
         public bool zlamanieWytrycha(int szansa)
         {
+            //statyczne
             Random rnd = new Random();
            
+            //co to jest 0 i 99 ?? moze zmienne const?
                 int los = rnd.Next(0, 99);
+            //return (los <= szansa);
             if (los > szansa)
             {
                 return false;
